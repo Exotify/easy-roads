b82dc83e093fe814271045f38738f360726946c6
Vamato <valeriomachado2008@gmail.com>
2023-06-09 11:08:03 +0100

    Movable line (test)

--- src/test.server.lua
+++ src/test.server.lua
@@ -4,6 +4,12 @@ local maths = require(game:GetService("ReplicatedStorage").Modules.Maths)
 print(maths)
 print("Server started!")
 
+local lines = Instance.new("Folder")
+lines.Name = "Lines"
+lines.Parent = workspace
+
+local ShouldRender = false
+
 local function randomVector()
    local rnd = Random.new()
    return Vector3.new(
@@ -12,11 +18,6 @@ local function randomVector()
       rnd:NextInteger(-50,50)
    )
 end
-
-local p1 = randomVector()
-local p2 = randomVector()
-local p3 = randomVector()
-local p4 = randomVector()
 
 local function display(pos,color)
    local part = Instance.new("Part")
@@ -29,37 +30,76 @@ local function display(pos,color)
       part.Size = Vector3.new(.5,.5,.5)
    end
 
+   part.TopSurface = Enum.SurfaceType.Smooth
+   part.BottomSurface = Enum.SurfaceType.Smooth
+
    part.Parent = workspace
+   return part
 end
 
-local function line(a:Vector3,b:Vector3)
+local function line(a:Vector3,b:Vector3,props:{string:any},width:number)
+   width = width or .7
    local res = maths.Line(a,b)
    local new = Instance.new("Part")
    new.CFrame = res.CFrame
-   new.Size = Vector3.new(.7,.7,res.Width)
+   new.Size = Vector3.new(width,width,res.Width)
    new.Anchored = true
    new.Color = Color3.new(0,1,0)
+   new.TopSurface = Enum.SurfaceType.Smooth
+   new.BottomSurface = Enum.SurfaceType.Smooth
+
+   for name,value in props or {} do
+      new[name] = value
+   end
 
    new.Parent = workspace
+   return new
 end
 
-display(p1,Color3.new(0,0,1))
-display(p2,Color3.new(0,0,1))
+local function render(v1:Vector3,v2:Vector3,v3:Vector3,v4:Vector3)
+   lines:ClearAllChildren()
+   local pos = {}
+   local n = 40
+   for i=0,n do
+      local point = maths.Lerp.Vector(maths.Lerp.Cubic,
+         i/n,
+         v1,v3,v4,v2
+      )
 
-display(p3,Color3.new(1,0,0))
-display(p4,Color3.new(1,0,0))
+      table.insert(pos,point)
+   end
 
-local pos = {}
-local n = 40
-for i=0,n do
-   local point = maths.Lerp.Vector(maths.Lerp.Cubic,
-      i/n,
-      p1,p3,p4,p2
-   )
+   for i = 1,#pos-1 do
+      line(pos[i],pos[i+1]).Parent = lines
+   end
 
-   table.insert(pos,point)
+   line(v1,v3,{["Color"]=Color3.new(0,1,1)},.3).Parent = lines
+   line(v2,v4,{["Color"]=Color3.new(0,1,1)},.3).Parent = lines
 end
 
-for i = 1,#pos-1 do
-   line(pos[i],pos[i+1])
+local p1 = display(randomVector(),Color3.new(0,0,1))
+local p2 = display(randomVector(),Color3.new(0,0,1))
+
+local p3 = display(randomVector(),Color3.new(1,0,0))
+local p4 = display(randomVector(),Color3.new(1,0,0))
+
+render(p1.Position,p2.Position,p3.Position,p4.Position)
+
+local function ehhh()
+   ShouldRender = true
+end
+
+p1:GetPropertyChangedSignal("Position"):Connect(ehhh)
+p2:GetPropertyChangedSignal("Position"):Connect(ehhh)
+p3:GetPropertyChangedSignal("Position"):Connect(ehhh)
+p4:GetPropertyChangedSignal("Position"):Connect(ehhh)
+
+--render loop
+while true do
+   if ShouldRender then
+      ShouldRender = false
+      render(p1.Position,p2.Position,p3.Position,p4.Position)
+   end
+
+   task.wait(.1)
 end
